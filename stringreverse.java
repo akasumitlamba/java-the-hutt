5c4c450d2b2c5e2b56cccf31b618835bac0c62a0
Sumit <kumara4749@gmail.com>
2022-07-17 02:18:50 +0530

    commit

--- stringreverse.java
+++ stringreverse.java
@@ -1,17 +1,35 @@
+// import java.io.*;
+// import java.util.Scanner;
+// public class stringreverse {
+//     public static void main (String[] args) {
+//         String nstr="";
+//         char ch;
+// 		Scanner scanner = new Scanner(System.in);
+// 		String text = scanner.nextLine();
+// 		char[] arr = text.toCharArray();
+// 	for (int i=0; i<text.length(); i++)
+// 	{
+// 		ch= text.charAt(i); //extracts each character
+// 		nstr= ch+nstr; //adds each character in front of the existing string
+// 	}
+// 	System.out.println(nstr);
+// 	}
+// }
+
+
+// approach 2--------------------------------------------------------------------------
 import java.io.*;
 import java.util.Scanner;
-public class stringreverse {
-    public static void main (String[] args) {
-        String nstr="";
-        char ch;
-		Scanner scanner = new Scanner(System.in);
-		String text = scanner.nextLine();
-		char[] arr = text.toCharArray();
-	for (int i=0; i<text.length(); i++)
-	{
-		ch= text.charAt(i); //extracts each character
-		nstr= ch+nstr; //adds each character in front of the existing string
-	}
-	System.out.println(nstr);
-	}
+public class stringreverse{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        String b="";
+        int ptr=a.length()-1;
+        while (ptr>=0){
+            b=b+a.charAt(ptr);
+            ptr--;
+        }
+        System.out.println(b);
+    }
 }
