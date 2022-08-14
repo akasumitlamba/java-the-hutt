be70b37ec5a3833e27074237517c0582de5e3998
sumit kumar <85924848+akasumitlamba@users.noreply.github.com>
2022-08-14 23:12:55 +0530

    Update userinput.java

--- userinput.java
+++ userinput.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 class userinput {
+
     public static void main(String[ ] args) {
         Scanner myVar = new Scanner(System.in);
         System.out.println(myVar.nextLine());        
